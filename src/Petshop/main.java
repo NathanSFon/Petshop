5e5a5bc499770caa7df8e160cb7628fad6f911d8
NathanSFon <nathannaels276@gmail.com>
2021-04-24 19:23:37 -0300

    teste

--- src/Petshop/main.java
+++ src/Petshop/main.java
@@ -4,5 +4,14 @@ public class main {
     public static void main(String[] args) {
         Cachorro c1 = new Cachorro("loop", "Macho", 8);
         Gato g1 = new Gato("tesli", "Femea", 1);
+		Pets p1 = new Pets("Doguinho", "m", 3);
+
+		p1.comer();
+		p1.ficarComFome();
+		c1.ficarComFome();
+		g1.ficarComFome();
+		p1.comer();
+		c1.comer();
+		g1.comer();
     }
 }
