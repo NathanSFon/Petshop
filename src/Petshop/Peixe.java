c011481a2d934236fa8c8c0b9dafb37cce82b89e
NathanSFon <nathannaels276@gmail.com>
2021-04-26 23:28:41 -0300

    novas classes

---
+++ src/Petshop/Peixe.java
@@ -0,0 +1,33 @@
+public class Peixe extends Pets{
+    protected String corEscama;
+    protected double tamanho;
+
+    public void alimentar_se() {
+        System.out.println(getNome() + " está se alimentando");
+    }
+    public void nadar(){
+        System.out.println(getNome() + " está nadando");
+    }
+
+    public void detalhes(){
+        System.out.println("Nome: " + getNome());
+        System.out.println("Idade: " + getIdade());
+        System.out.println("Tamanho em cm: " + getTamanho());
+        System.out.println("Cor da escama: " + getCorEscama() + "\n");
+
+    }
+
+    public String getCorEscama() {
+        return corEscama;
+    }
+    public void setCorEscama(String corEscama) {
+        this.corEscama = corEscama;
+    }
+    public double getTamanho(){
+        return tamanho;
+    }
+    public void setTamanho(double tamanho){
+        this.tamanho = tamanho;
+    }
+    
+}
---
+++ src/Petshop/Reptil.java
@@ -0,0 +1,35 @@
+public class Reptil extends Pets{
+    protected String corPele;
+    protected double comprimento;
+
+    public void alimentar_se() {
+        System.out.println(getNome() + " está se alimentando");
+    }
+    public void mergulhar(){
+        System.out.println(getNome() + " está mergulhando");
+    }
+
+
+    public void detalhes(){
+        System.out.println("Nome: " + getNome());
+        System.out.println("Sexo: " + getSexo());
+        System.out.println("Idade: " + getIdade());
+        System.out.println("Peso: " + getPeso());
+        System.out.println("Comprimento: " + getComprimento());
+        System.out.println("Cor da pele: " + getCorPele() + "\n");
+
+    }
+
+    public String getCorPele() {
+        return corPele;
+    }
+    public void setCorPele(String corPele) {
+        this.corPele = corPele;
+    }
+    public double getComprimento(){
+        return comprimento;
+    }
+    public void setComprimento(double comprimento){
+        this.comprimento = comprimento;
+    }
+}
